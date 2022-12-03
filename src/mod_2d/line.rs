7620f7bfa8cbc19f0801a5675c66d76bded89750
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2022-12-03 16:23:00 -0500

    swap out for generics

--- src/mod_2d/line.rs
+++ src/mod_2d/line.rs
@@ -4,23 +4,22 @@
 
 use crate::mod_2d::point::Point2D;
 
+use num_traits::Float;
+
 #[derive(Debug, Default)]
-pub struct Line2D {
-    start: Point2D,
-    end: Point2D
+pub struct Line2D<T> {
+    start: Point2D<T>,
+    end: Point2D<T>,
 }
 
-impl Line2D {
-    pub fn new(start: Point2D, end: Point2D ) -> Self {
-        Line2D {
-            start,
-            end
-        }
+impl<T: Float> Line2D<T> {
+    pub fn new(start: Point2D<T>, end: Point2D<T>) -> Self {
+        Line2D { start, end }
     }
 
-    pub fn distance(&self) -> f32 {
-        let diff_x: f32 = *self.end.x() - *self.start.x();
-        let diff_y: f32 = *self.end.y() - *self.start.y();
+    pub fn distance(&self) -> T {
+        let diff_x: T = *self.end.x() - *self.start.x();
+        let diff_y: T = *self.end.y() - *self.start.y();
 
         (diff_x.powi(2) + diff_y.powi(2)).sqrt()
     }
@@ -32,10 +31,10 @@ mod tests {
 
     #[test]
     fn distance() {
-        let point_1 = Point2D::new(0.0, 0.0);
-        let point_2 = Point2D::new(1.0, 1.0);
+        let point_1: Point2D<f32> = Point2D::new(0.0, 0.0);
+        let point_2: Point2D<f32> = Point2D::new(1.0, 1.0);
 
-        let line = Line2D::new(point_1, point_2);
+        let line: Line2D<f32> = Line2D::new(point_1, point_2);
 
         assert_eq!(line.distance(), 2.0_f32.sqrt());
     }
--- src/mod_2d/point.rs
+++ src/mod_2d/point.rs
@@ -3,32 +3,29 @@
 //! This module contains basic methods to manipulate a 2D point
 
 #[derive(Debug, Default)]
-pub struct Point2D {
-    x: f32,
-    y: f32
+pub struct Point2D<T> {
+    x: T,
+    y: T,
 }
 
-impl Point2D {
-    pub fn new(x: f32, y: f32) -> Self {
-        Point2D {
-            x,
-            y
-        }
+impl<T> Point2D<T> {
+    pub fn new(x: T, y: T) -> Self {
+        Point2D { x, y }
     }
 
-    pub fn x(&self) -> &f32 {
+    pub fn x(&self) -> &T {
         &self.x
     }
 
-    pub fn y(&self) -> &f32 {
+    pub fn y(&self) -> &T {
         &self.y
     }
 
-    pub fn x_mut(&mut self) -> &mut f32 {
+    pub fn x_mut(&mut self) -> &mut T {
         &mut self.x
     }
 
-    pub fn y_mut(&mut self) -> &mut f32 {
+    pub fn y_mut(&mut self) -> &mut T {
         &mut self.y
     }
 }
@@ -39,14 +36,14 @@ mod tests {
 
     #[test]
     fn x() {
-        let new_point = Point2D::default();
+        let new_point: Point2D<f32> = Point2D::default();
 
         assert_eq!(new_point.x, 0.0);
     }
 
     #[test]
     fn y() {
-        let new_point = Point2D::default();
+        let new_point: Point2D<f32> = Point2D::default();
 
         assert_eq!(new_point.y, 0.0);
     }
