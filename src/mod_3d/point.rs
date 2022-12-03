7820a5433e75b73c978ca26df69ce60635bb202a
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2022-12-03 16:56:52 -0500

    add docs for point3d

--- src/mod_3d/point.rs
+++ src/mod_3d/point.rs
@@ -2,6 +2,21 @@
 //!
 //! This module contains basic methods to manipulate a 3D point
 
+//! 3D Point
+//!
+//! This module contains basic methods to manipulate a 3D point
+
+/// Generic 2D Point
+///
+/// Can be used with [`f32`] or [`f64`]
+///
+/// # Examples
+///
+/// ```no_run
+/// use bdrk_geometry::mod_3d::Point3D;
+///
+/// let new_point = Point3D::new(0, 0, 0);
+/// ```
 #[derive(Debug, Default)]
 pub struct Point3D<T> {
     x: T,
